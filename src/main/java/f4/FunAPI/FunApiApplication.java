a3d0584f0a5154003e667e3722be278f8818d3d0
Tristan Fitzwater <tristanfitzwater@gmail.com>
2023-02-02 23:02:10 -0500

    Init

---
+++ src/main/java/f4/FunAPI/FunApiApplication.java
@@ -0,0 +1,13 @@
+package f4.FunAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f4/FunAPI/FunApiApplicationTests.java
@@ -0,0 +1,13 @@
+package f4.FunAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
